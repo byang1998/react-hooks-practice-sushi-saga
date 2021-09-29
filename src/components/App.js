50decd3916763a29ac25dfd545a994a4a4c45e04
Ben Yang <benyang2020@gmail.com>
2021-09-28 21:53:06 -0400

    commit tree 66249e7aac6f379044803e6e43db8db5f0049255

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushis, setSushi] = useState ([])
+  const [] = useState()
+
+  useEffect (async ()  => {
+    const res = await fetch(API)
+    const sushiData = await res.json()
+
+  setSushi(sushiData)
+
+  }, [])
+
   return (
     <div className="app">
       <SushiContainer />
